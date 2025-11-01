b4c32ea54cebd71ac3e2cb5d8c6e5e6604545133
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-10-31 19:38:54 -0500

    Update app.py que deberia servir
    https://www.youtube.com/watch?v=HQLRPWi2SeA

--- app.py
+++ app.py
@@ -1,42 +1,14 @@
-from flask import Flask, request, abort, jsonify
-import os
-from datetime import datetime
-import json
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-port = int(os.getenv('PORT', 10000))
-verify_token = os.getenv('VERIFY_TOKEN')
-
-@app.route('/', methods=['GET'])
-def verify_webhook():
-    mode = request.args.get('hub.mode')
-    challenge = request.args.get('hub.challenge')
-    token = request.args.get('hub.verify_token')
-
-    if mode == 'subscribe' and token == verify_token:
-        print('WEBHOOK VERIFIED')
-        return challenge, 200
-    else:
-        abort(403)
-
-@app.route('/handle_webhook', methods=['POST'])
-def handle_webhook():
+@app.route('/webhook', methods=['POST'])
+def webhook():
     if request.method == 'POST':
-        try:
-            data = request.get_json()
-            if data:
-                print("Received webhook data:")
-                print(jsonify(data).get_data(as_text=True))
-                # Process the webhook data here (e.g., save to database, trigger other actions)
-                return jsonify({"status": "success", "message": "Webhook received"}), 200
-            else:
-                return jsonify({"status": "error", "message": "No JSON data received"}), 400
-        except Exception as e:
-            return jsonify({"status": "error", "message": f"Error processing webhook: {e}"}), 500
+        print(request.json)
+        return 'succes', 200
     else:
-        return jsonify({"status": "error", "message": "Method not allowed"}), 405
+        abort(400)
 
-if __name__ == '__main__':
-    print(f"\nListening on port {port}\n")
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '--main__':
+    app.rin()
