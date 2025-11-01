a6fb945287182aae6868d3f6ba20f33d5d06cc05
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-10-31 22:28:38 -0500

    Update app.py no se comento bien lo que no queria

--- app.py
+++ app.py
@@ -28,24 +28,24 @@ def webhook():
         print("Received webhook data:", data)
 
         # Process the data (e.g., store in a database, send notifications)
-        ""url = "https://graph.facebook.com/v22.0/845427305315318/messages"
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer EAFahY2vnTjUBP9LO96F0UYZCZCY0uCjZALa0cZBbLFiyAHsxuahw40ZCMZBYoB6t26WDA2PZCLoUASJcX1DpG7k8B0OLC1f94GfIj0qSBtiUwWO0q66GJZAPfrTVKE0ohkZA9UWvwPC6POccHthgbEpxWyPDrVnWxU1WRJb2oLwmHZB8pYHemMtbeZAVWGBaKOaiOKoYV1lQK9YfrcDwg0KMrKVZBYy1EwkZBFGumzpxjIl5iLWkkrdoibNHjU8Btp838sKx2pZAKFjSUGqEuUpjrkvRV4'
-        }
-        payload = json.dumps({
-            "messaging_product": "whatsapp",
-            #"to": "573202965268",
-            "to": "573202965268",
-            "type": "template",
-            "template": {
-                "name": "hello_world",
-                "language": {
-                    "code": "en_US"
-                }
-            }
-        })
-        response = requests.request("POST", url, headers=headers, data=payload)""
+        #url = "https://graph.facebook.com/v22.0/845427305315318/messages"
+        #headers = {
+        #    'Content-Type': 'application/json',
+        #    'Authorization': 'Bearer EAFahY2vnTjUBP9LO96F0UYZCZCY0uCjZALa0cZBbLFiyAHsxuahw40ZCMZBYoB6t26WDA2PZCLoUASJcX1DpG7k8B0OLC1f94GfIj0qSBtiUwWO0q66GJZAPfrTVKE0ohkZA9UWvwPC6POccHthgbEpxWyPDrVnWxU1WRJb2oLwmHZB8pYHemMtbeZAVWGBaKOaiOKoYV1lQK9YfrcDwg0KMrKVZBYy1EwkZBFGumzpxjIl5iLWkkrdoibNHjU8Btp838sKx2pZAKFjSUGqEuUpjrkvRV4'
+        #}
+        #payload = json.dumps({
+        #    "messaging_product": "whatsapp",
+        #    #"to": "573202965268",
+        #    "to": "573202965268",
+        #    "type": "template",
+        #    "template": {
+        #        "name": "hello_world",
+        #        "language": {
+        #            "code": "en_US"
+        #        }
+        #    }
+        #})
+        #response = requests.request("POST", url, headers=headers, data=payload)
         # ...
 
         return jsonify({"status": "EVENT_RECEIVED"}), 200
