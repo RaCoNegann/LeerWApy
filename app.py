87eda3320013d9b2699bf7d1105d1a03b50dcbca
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-10-31 20:35:31 -0500

    Update app.py  stringify
    A ver si se ve el cuerpo aún es 200

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, abort
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def handle_webhook():
     if request.method == 'POST':
-        print(request.get_json())
+        #print(request.get_json())
+        json_string = json.dumps(request.get_json())
+        print(json_string)
         return 'succes', 200
     else:
         abort(400)
