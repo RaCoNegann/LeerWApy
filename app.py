1f7713406d4a245c618fcab6500e66fda5221494
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-11-05 20:58:39 -0500

    Update app.py js2py console

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, js2py
 #import requests
 #import json
 
@@ -18,6 +18,9 @@ def webhook():
             #f.write("This is line 1.\n")
 
         if mode and token and mode == "subscribe" and token == VERIFY_TOKEN:
+            js_code_string = "console.log('aaaaaaaabb');"
+            result = js2py.eval_js(js_code_string)
+            
             print("WEBHOOK_VERIFIED")
             return challenge, 200
         else:
