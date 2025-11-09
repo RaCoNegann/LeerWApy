91f56f5b4e38d2e53ae2a7ea164e691de47082a9
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-11-09 18:41:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,44 +10,13 @@ logger = logging.getLogger(__name__)
 VERIFY_TOKEN = "tokenenrender"
 # Configurar el logger
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-@app.route("/", methods=["GET", "POST"])
-def webhook():
-    if request.method == "GET":
-        # Handle webhook verification
-        mode = request.args.get("hub.mode")
-        token = request.args.get("hub.verify_token")
-        challenge = request.args.get("hub.challenge")
-        #logger.info('El programa ha comenzado con éxito')
-        #logger.warning('Se ha detectado una configuración no óptima')
-        #logger.error('Se ha producido un error al procesar los datos')
-        #logger.critical('El sistema ha fallado críticamente')
-        if mode and token and mode == "subscribe" and token == VERIFY_TOKEN:
-            print("WEBHOOK_VERIFIED")
-            logger.debug('WEBHOOK_VERIFIED')
-            return challenge, 200
-        else:
-            #print("VERIFICATION_FAILED")
-            logger.error('VERIFICATION_FAILED')
-            return jsonify({"status": "error", "message": "Verification failed"}), 403
-
-    elif request.method == "POST":
-        # Handle incoming event notifications
-        data = request.json
-        #print("Received webhook data:", data)
-        logger.warning(data)
-        texto = (data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"])
-        numeroUsuario= data["entry"][0]["changes"][0]["value"]["messages"][0]["from"]
-        enviar_mensaje(texto,numeroUsuario)
-        logger.info('Deberia enviar')
-        return jsonify({"status": "EVENT_RECEIVED"}), 200
-    
 def enviar_mensaje(f_texto,f_cel):
     url = "https://graph.facebook.com/v22.0/845427305315318/messages"
     with open('tokenAPIWA.txt', 'r') as archivo:
         try:
             tokenApiWA = archivo.read()
             tokenApiWA = tokenApiWA[:-2]
+            logger.warning(tokenApiWA)
             #print(type(tokenApiWA))
         finally:
             archivo.close()
@@ -96,6 +65,36 @@ def enviar_mensaje(f_texto,f_cel):
     print(response.text)
     del f_cel, tokenApiWA
 
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        # Handle webhook verification
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        #logger.info('El programa ha comenzado con éxito')
+        #logger.warning('Se ha detectado una configuración no óptima')
+        #logger.error('Se ha producido un error al procesar los datos')
+        #logger.critical('El sistema ha fallado críticamente')
+        if mode and token and mode == "subscribe" and token == VERIFY_TOKEN:
+            print("WEBHOOK_VERIFIED")
+            logger.debug('WEBHOOK_VERIFIED')
+            return challenge, 200
+        else:
+            #print("VERIFICATION_FAILED")
+            logger.error('VERIFICATION_FAILED')
+            return jsonify({"status": "error", "message": "Verification failed"}), 403
+
+    elif request.method == "POST":
+        # Handle incoming event notifications
+        data = request.json
+        #print("Received webhook data:", data)
+        logger.warning(data)
+        texto = (data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"])
+        numeroUsuario= data["entry"][0]["changes"][0]["value"]["messages"][0]["from"]
+        enviar_mensaje(texto,numeroUsuario)
+        logger.info('Deberia enviar')
+        return jsonify({"status": "EVENT_RECEIVED"}), 200
 
 if __name__ == "__main__":
     #app.run(debug=True, port=5000) # Run on port 5000 for local testing - Geneta como un timeout*
