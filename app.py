fa330a8d26dbe757dc7735da6ef2a5b707310fc1
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-11-09 14:23:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,62 +18,83 @@ def webhook():
         mode = request.args.get("hub.mode")
         token = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
-        
-        logger.debug('Este es un mensaje de depuración')
         #logger.info('El programa ha comenzado con éxito')
         #logger.warning('Se ha detectado una configuración no óptima')
         #logger.error('Se ha producido un error al procesar los datos')
         #logger.critical('El sistema ha fallado críticamente')
-        
         if mode and token and mode == "subscribe" and token == VERIFY_TOKEN:
             print("WEBHOOK_VERIFIED")
+            logger.debug('WEBHOOK_VERIFIED')
             return challenge, 200
         else:
-            print("VERIFICATION_FAILED")
+            #print("VERIFICATION_FAILED")
+            logger.error('ERIFICATION_FAILED')
             return jsonify({"status": "error", "message": "Verification failed"}), 403
 
     elif request.method == "POST":
         # Handle incoming event notifications
         data = request.json
-        print("Received webhook data:", data)
-        
-        logger.warning(data)
+        #print("Received webhook data:", data)
+        logger.warning('Received webhook data: '+data)
         texto = (data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"])
-
-        url = "https://graph.facebook.com/v22.0/845427305315318/messages"
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer EAFahY2vnTjUBPwBTXkZBZBwwbaokAkHQ0hstItsZBCEEVXlSlbK7HQNwDT91LJbZAylIZAG8WufrsZBafzQpkZC8jjIMZC40OJQw3yRswtbQdkxQpUAhDTm9fOzb6H2lroeZB5cYWZCuy56vhNeIjLNvAasPsAeWwh5CBZCXGNFkdFz7bD0wkyq7Gwc1sRGYsZAXjvCRwVOfefZAJNZBbEitAeIOw9s9jXrqW6IC3IlPJgjfqS2YnWl3LW0GyZAZCSphj8SpQnfh6c1tFvPgVw6CPKlWKJLZBLQZDZD'
-        }
-        if texto == "this is a text message":
-            payload = json.dumps({
-                "messaging_product": "whatsapp",
-                #"to": "573202965268",
-                "to": "573057781203",
-                "type": "template",
-                "template": {
-                    "name": "hello_world",
-                    "language": {
-                        "code": "en_US"
-                    }
-                }
-            })
-            logger.error('Entro en el if del texto')
-        else:
-            payload = json.dumps({
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "573057781203",
-                "type": "text",
-                "text": {
-                    "preview_url": False,
-                    "body": "text-message-content"
-                }
-            })
-            logger.critical('Entro en else del texto')
-        response = requests.request("POST", url, headers=headers, data=payload)
+        numeroUsuario= data["entry"][0]["changes"][0]["value"]["messages"][0]["from"]
+        enviar_mensaje(texto,numeroUsuario)
         logger.info('Deberia enviar')
         return jsonify({"status": "EVENT_RECEIVED"}), 200
+    
+def enviar_mensaje(f_texto,f_cel):
+    url = "https://graph.facebook.com/v22.0/845427305315318/messages"
+    with open('tokenAPIWA.txt', 'r') as archivo:
+        try:
+            tokenApiWA = archivo.read()
+            #print(type(tokenApiWA))
+        finally:
+            archivo.close()
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+tokenApiWA
+    }
+    if f_texto == "this is a text message":
+        typeAPIWA ="template"
+        typeJSONAPIWA = json.dumps({
+            "name": "hello_world",
+            "language": {
+                "code": "en_US"
+                }
+            })
+    elif "oli" in f_texto or "oli" in f_texto:
+        recipient_type = "individual"
+        typeAPIWA ="text"
+        typeJSONAPIWA = json.dumps({
+            "preview_url": False,
+            "body": "Cual 'oli'\n*Madure*"})
+    else:
+        recipient_type = "individual"
+        typeAPIWA ="text"
+        typeJSONAPIWA = json.dumps({
+            "preview_url": False,
+            "body": "text-message-content\nSaltoLinea"})
+        
+    if 'recipient_type' in locals():
+        payload = json.dumps({
+            "messaging_product": "whatsapp",
+            "recipient_type": recipient_type,
+            "to": f_cel,
+            "type": typeAPIWA,
+            typeAPIWA: typeJSONAPIWA
+        })
+    else:
+        payload = json.dumps({
+            "messaging_product": "whatsapp",
+            #"to": "573202965268",
+            "to": f_cel,
+            "type": typeAPIWA,
+            typeAPIWA: typeJSONAPIWA
+            })
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+    del f_cel, tokenApiWA
+
 
 if __name__ == "__main__":
     #app.run(debug=True, port=5000) # Run on port 5000 for local testing - Geneta como un timeout*
