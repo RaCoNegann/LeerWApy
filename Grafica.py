69eec95e1dd5c6b0f79b5e151afebbc1aac901e9
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-11-23 21:17:17 -0500

    Update Grafica.py

--- Grafica.py
+++ Grafica.py
@@ -1,8 +1,9 @@
 from datetime import date
 import pandas as pd
 import matplotlib.pyplot as plt
+import logging
 
-
+logger = logging.getLogger(__name__)
 def guardar_dato(texto,numeroUsuario):
     today = date.today()
     formatted_date = today.strftime('%d/%m/%y')
@@ -13,11 +14,14 @@ def guardar_dato(texto,numeroUsuario):
 def mas_datos(numeroUsuario):
     datos_lista = [['10/10/25',100], ['18/11/25',213], ['18/11/25',400],['20/11/25',200], ['21/11/25',214], ['22/11/25',500]]
     df = pd.DataFrame(datos_lista, columns=['Fecha', 'Data'])
+    logger.info(df)
     df.to_csv('mi_archivo'+numeroUsuario+'.csv', index=False)
+    logger.info("Escribio")
 
 def grafica(numeroUsuario):
     df3 = pd.read_csv('mi_archivo'+numeroUsuario+'.csv')
     df3.plot(x='Fecha', y='Data', kind='bar')
+    logger.warning(df3)
     #Personalizar las etiquetas y el título (opcional)
     plt.xlabel("Fecha")
     plt.ylabel("Valores")
