1fa33f50d889ba49974a159372f1b590c11c7789
RaCoNegann <69557732+RaCoNegann@users.noreply.github.com>
2025-11-23 20:53:28 -0500

    Update EnviarMensaje.py

--- EnviarMensaje.py
+++ EnviarMensaje.py
@@ -7,8 +7,7 @@ def enviar_mensaje(payload):
     with open('tokenAPIWA.txt', 'r') as archivo:
         try:
             tokenApiWA = archivo.read()
-            tokenApiWA = tokenApiWA[:-1]
-            logger.info(tokenApiWA + " - to\nken")
+            #print((tokenApiWA))
         finally:
             archivo.close()
     headers = {
@@ -18,3 +17,41 @@ def enviar_mensaje(payload):
     response = requests.request("POST", url, headers=headers, data=payload)
     del tokenApiWA
     logger.warning(response.text)
+
+def enviar_imagen(numeroUsuario):
+    url = "https://graph.facebook.com/v22.0/845427305315318/media"
+    payload = {'messaging_product': 'whatsapp'}
+    files=[
+    ('file',('i_grafico.png',open("i_grafico"+numeroUsuario+".png",'rb'),'image/png'))
+    ]
+    with open('tokenAPIWA.txt', 'r') as archivo:
+        try:
+            tokenApiWA = archivo.read()
+        finally:
+            archivo.close()
+    headers = {
+    'Authorization': 'Bearer '+tokenApiWA
+    }
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    id=response.text.split('"')
+    #print(id[3])
+    # directorio_actual = os.getcwd()
+    # ruta_imagen = directorio_actual+"\\"+"i_grafico"+numeroUsuario+".png"
+    # print(ruta_imagen)
+    # os.remove(ruta_imagen)
+    url2 = "https://graph.facebook.com/v22.0/845427305315318/messages"
+    payload2 = json.dumps({
+    "messaging_product": "whatsapp",
+    "recipient_type": "individual",
+    "to": numeroUsuario,
+    "type": "image",
+    "image": {
+        "id": id[3]
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+tokenApiWA
+    }
+    response2 = requests.request("POST", url2, headers=headers, data=payload2)
+    logger.warning(response2.text)
